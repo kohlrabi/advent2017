3ff3e193bb54f2b924f5bb32a686202e28378ff9
Christoph Terasa <christoph@kohlio.de>
2017-12-03 21:33:59 +0100

    Day 1: Added part 2.

--- py/day01.py
+++ py/day01.py
@@ -9,11 +9,12 @@ def part1(l):
     return s
 
 def part2(l):
-    pass
+    N = len(l)
+    s = 2*sum((int(p) for p, n in zip(l[:N//2], l[N//2:]) if p == n))
+    return s
 
 
 if __name__ == '__main__':
     l = fileinput.input()[0].strip()
     print("Part 1: {}".format(part1(l)))
-
-
+    print("Part 2: {}".format(part2(l)))
