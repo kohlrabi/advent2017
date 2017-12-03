86c1b28828ec805a829bb786d1c16218360085e7
Christoph Terasa <christoph@kohlio.de>
2017-12-03 22:51:39 +0100

    Day 2: Added part 2.

--- py/day02.py
+++ py/day02.py
@@ -8,12 +8,25 @@ def part1(rows):
 
     return s
 
-def part2(l):
-    pass
+def part2(rows):
+    s = 0
+    for row in rows:
+        for i, elem in enumerate(row):
+            for j in range(i+1, len(row)):
+                m = max(elem, row[j])
+                mi = min(elem, row[j])
+                if m % mi == 0:
+                    s += m//mi
+                    break
+
+    return s
+
+
+    
 
 
 if __name__ == '__main__':
     l = fileinput.input()
-    rows = (map(int, ll.strip().split()) for ll in l)
+    rows = [map(int, ll.strip().split()) for ll in l]
     print("Part 1: {}".format(part1(rows)))
-    #print("Part 2: {}".format(part2(l)))
+    print("Part 2: {}".format(part2(rows)))
