362e817d4824a3306d77b27cc3c010ded3e4cd53
Christoph Terasa <christoph@kohlio.de>
2017-12-04 09:54:26 +0100

    Started implementing Spiralmem for part 2.

--- py/day03.py
+++ py/day03.py
@@ -3,6 +3,41 @@
 import fileinput
 
 
+class Spiralmem:
+    def __init__(self, d=1):
+        if not d%2:
+            raise ValueError("Spiralmem dimension needs to be odd.")
+        self.d = d
+        self.mem = [[0 for i in range(d)] for j in range(d)]
+
+    def __str__(self):
+        return str(self.mem)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def realloc(self, d=1):
+        new = Spiralmem(d)
+
+        diff = self.d - new.d
+
+        if diff > 0:
+            for i in range(0, new.d):
+                for j in range(0, new.d):
+                    new.mem[i][j] = self.mem[i+diff//2][j+diff//2]
+        elif diff < 0:
+            diff *= -1
+            for i in range(0, self.d):
+                for j in range(0, self.d):
+                    new.mem[i+diff//2][j+diff//2] = self.mem[i][j]
+        else:
+            return self
+
+        self.mem = new.mem
+        return self
+
+
+
 def part1(n):
     r = [0]
     o = 1
